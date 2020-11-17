fe4834f8fb3333d74e2b757e11765c2624e4b581
M.C. Jorge Octavio Guzmán <jorge.gs1@tuxtla.tecnm.mx>
2020-11-16 19:27:51 -0600

    Relationships

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -9,20 +9,23 @@ class Producto extends Model
 {
     public $timestamps = false;
     protected $fillable = ['nombre','descripcion','precio','imagen','usuario_id','categoria_id','concesionado'];
-
-    public function usuario(){
-        return $this->belongsTo('App\Models\Usuario');
-    }
+    
     public function propietario(){
-        return $this->belongsTo('App\Models\Usuario','usuario_id');
+        return $this->hasOne('App\Models\Usuario','id','usuario_id');
     }
+
     public function categoria(){
-        return $this->belongsTo('App\Models\Categoria');
+        return $this->hasMany('App\Models\Categoria','id','categoria_id');
     }
+
     public function estaConcesionado(){
         if($this->concesionado) 
             return "SI";
         else
             return "No";
+    }
+
+    public function usuario(){
+        return $this->belongsTo('App\Models\Usuario');        
     }
 }
